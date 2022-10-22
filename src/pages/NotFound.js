59a13ba13e4749d23a31600cc7427d9fc2cc536b
Mina <mina.fawzy@sbdinc.com>
2022-10-22 02:21:03 +0200

    use navigate

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 1000);
+  }, []);
   return (
     <div>
       <h1>Not Found</h1>
